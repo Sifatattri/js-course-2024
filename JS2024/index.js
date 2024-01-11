53c1f6a8094ed60f712a46e61ba03bb8b09ba15f
Sifatattri <124069435+Sifatattri@users.noreply.github.com>
2024-01-11 12:49:16 +0000

    additions

--- JS2024/index.js
+++ JS2024/index.js
@@ -11,3 +11,19 @@ let age=22;
 let isLoggedIn= false;
 console.log(typeof null);
 console.log(typeof undefined);
+const id=Symbol('123')
+const anotherid = Symbol('123')
+console.log(id === anotherid);
+let myLaptop="macBookPro"
+let anotherName=myLaptop
+console.log(myLaptop);
+console.log(anotherName);
+anotherName="myPhone";
+console.log(anotherName);
+let userOne ={
+email:"sifat@gmail.com",
+upiId:"sifat@google.ok"
+}
+let userTwo=userOne
+console.log(userTwo.email);
+console.log(userOne.upiId);
